c740d359fc332e74b923b73e58a22c03bfe75d50
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-30 22:22:03 +0000

    added values to object properties

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-// var addEntry = document.querySelector('.entry-bttn');
-// var modalContainer = document.querySelector('#modal-container');
-// var entryForm = document.querySelector('form');
-// var date = document.querySelector('#day');
-// var time = document.querySelector('#time');
-// var description = document.querySelector('textarea');
-// var data = {
-//   entries: [],
-//   editing: null,
-//   nextEntryId: 1
-// };
+var addEntry = document.querySelector('.entry-bttn');
+var modalContainer = document.querySelector('#modal-container');
+var entryForm = document.querySelector('form');
+var date = document.querySelector('#day');
+var time = document.querySelector('#time');
+var description = document.querySelector('textarea');
+var data = {
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
 
-// addEntry.addEventListener('click', function (e) {
-//   modalContainer.className = 'container';
-// });
+addEntry.addEventListener('click', function (e) {
+  modalContainer.className = 'container';
+});
 
-// entryForm.addEventListener('click', function (e) {
-//   e.preventDefault();
-//   var submitButton = entryForm.querySelector('button[type="submit"]');
-//   if (e.target === submitButton) {
-//     modalContainer.className += ' hidden';
-//   }
-//   date.value;
-//   var entry = {
-//     date: date.value,
-//     time: time.value,
-//     description: description.value
-//   };
+entryForm.addEventListener('click', function (e) {
+  e.preventDefault();
+  var submitButton = entryForm.querySelector('button[type="submit"]');
+  if (e.target === submitButton) {
+    modalContainer.className += ' hidden';
+    var entry = {
+      date: date.value,
+      time: time.value,
+      description: description.value
+    };
+    data.nextEntryId++;
+    data.entries.push(entry);
 
-// });
+  }
+
+});
