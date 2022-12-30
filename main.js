d3a959bdce6c53ce0067048add127f9295920a46
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-30 20:18:14 +0000

    Created add event listener for the form to prevent from refreshing when submitting.

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 var addEntry = document.querySelector('.entry-bttn');
+var modalContainer = document.querySelector('#modal-container');
+var entryForm = document.querySelector('form');
+// var date = document.querySelector('#day');
+// var time = document.querySelector('#time');
+// var entry = {};
+
 addEntry.addEventListener('click', function (e) {
-  if(e.target)
+  modalContainer.className = 'container';
 });
+
+entryForm.addEventListener('click', function (e) {
+  e.preventDefault();
+
+});
+
+// console.log(date);
