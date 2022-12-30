5cdcea134e247f02918ca7267d47323ab7cba457
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-30 22:04:58 +0000

    Added data obj and entry obj inside entryForm event listener

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-var addEntry = document.querySelector('.entry-bttn');
-var modalContainer = document.querySelector('#modal-container');
-var entryForm = document.querySelector('form');
+// var addEntry = document.querySelector('.entry-bttn');
+// var modalContainer = document.querySelector('#modal-container');
+// var entryForm = document.querySelector('form');
 // var date = document.querySelector('#day');
 // var time = document.querySelector('#time');
-// var entry = {};
+// var description = document.querySelector('textarea');
+// var data = {
+//   entries: [],
+//   editing: null,
+//   nextEntryId: 1
+// };
 
-addEntry.addEventListener('click', function (e) {
-  modalContainer.className = 'container';
-});
+// addEntry.addEventListener('click', function (e) {
+//   modalContainer.className = 'container';
+// });
 
-entryForm.addEventListener('click', function (e) {
-  e.preventDefault();
-  var submitButton = entryForm.querySelector('button[type="submit"]');
-  if (e.target === submitButton) {
-    modalContainer.className += ' hidden';
-  }
-});
+// entryForm.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   var submitButton = entryForm.querySelector('button[type="submit"]');
+//   if (e.target === submitButton) {
+//     modalContainer.className += ' hidden';
+//   }
+//   date.value;
+//   var entry = {
+//     date: date.value,
+//     time: time.value,
+//     description: description.value
+//   };
+
+// });
